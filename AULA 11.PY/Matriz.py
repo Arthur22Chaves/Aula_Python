31f245aee7cca6ab833c767ab287183927961ee4
Arthur22Chaves <165737263+Arthur22Chaves@users.noreply.github.com>
2024-04-24 22:00:21 -0300

    Aula 10 e 11
    Exercicios e exemplos das aulas 10 e 11

---
+++ AULA 11.PY/Matriz.py
@@ -0,0 +1,11 @@
+#crianção de matriz 2x2
+
+matriz =[
+    ['caio','maria'],
+    ['lorena', 'julia']
+]
+a = matriz [0][1]
+#Impressão da matriz
+print("matríz: ")
+for linha in matriz:
+    print(linha)
